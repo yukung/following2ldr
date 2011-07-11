fda77e290a4c38ef242504d5f2aa88bc282c94bc
yukung <yukung.i@gmail.com>
2011-07-11 13:30:38 +0000

    Initial import.
    git-svn-id: https://subversion.assembla.com/svn/following2ldr/trunk@3 9c3a0645-ecbb-4679-b7f1-f25784741b34

---
+++ src/main/java/org/yukung/following2ldr/App.java
@@ -0,0 +1,13 @@
+package org.yukung.following2ldr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
